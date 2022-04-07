441f9b88ee41f66798e65936a39c2ad5e9bbcc69
harrtedwin <mkihara.edwin@gmail.com>
2022-04-07 02:41:22 -0700

    minor changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+*
+** Description: main - Prints out a string to stdout.
+*
+** Return: Always 0 (Success)
+*
+*/
 
 int main(void)
 
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
