bec1ba8005c6e3fb97382eeb48e934c45b64fe0e
harrtedwin <mkihara.edwin@gmail.com>
2022-04-07 02:45:50 -0700

    minor changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
 /**
-*
-** Description: main - Prints out a string to stdout.
-*
-** Return: Always 0 (Success)
-*
+* Description: main - Prints out a string to stdout.
+* Return: 0 if success
 */
 
 int main(void)
